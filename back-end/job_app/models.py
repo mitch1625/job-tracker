24777abd7a8468369ce5d7db4f79a8f181393c79
eric <ericmitchell1625@hotmail.com>
2024-04-26 11:21:37 -0400

    user_app and job_app models added

--- back-end/job_app/models.py
+++ back-end/job_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class JobsMananger(models.Model):
+  company_name = models.CharField(max_length=70),
+  job_title = models.CharField(max_length=50),
+  location = models.CharField(max_length=70),
+  job_notes = models.CharField(),
+  # application_status = models.Value() what to add as model
+  applied_date = models.DateField(default=datetime.date.today)
+  
--- back-end/user_app/models.py
+++ back-end/user_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import validate_email
 
-# Create your models here.
+class User(models.Model):
+  email = models.EmailField(
+    max_length=50,
+    unique=True,
+    verbose_name="email address",
+    validators=[validate_email])
+  
+  USERNAME_FIELD = "email"
+  REQUIRED_FIELDS = []
+
