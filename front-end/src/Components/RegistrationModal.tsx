8d37829a53ea69c8a4f615efff21775a4cf5eba2
Eric <ericmitchell1625@hotmail.com>
2024-04-30 15:56:24 -0400

    login modal rendering added

--- front-end/src/Components/RegistrationModal.tsx
+++ front-end/src/Components/RegistrationModal.tsx
@@ -1,7 +1,21 @@
-const RegistrationModal = () => {
+import { Modal } from "react-bootstrap"
+import React, { useState} from "react";
+
+
+// how to define props?
+const RegistrationModal = (props:any): JSX.Element => {
+
 
   return (
     <>
+      <Modal show={props.show}  animation={false}>
+        <Modal.Header closeButton onClick={props.handleClose}>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body onClick={()=>console.log(props)}>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
       <form>
         <input placeholder="Enter email" name='email'/>
         <input placeholder="Enter password" name='password'/>
